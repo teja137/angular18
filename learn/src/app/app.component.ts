8ff238d12ff2277ffefc3cb3245071124280a402
Prabhu Teja Pamula <94112233+teja137@users.noreply.github.com>
2024-06-28 04:14:10 -0700

    learned-atribute-directives

--- learn/src/app/app.component.ts
+++ learn/src/app/app.component.ts
@@ -4,11 +4,19 @@ import { AddEmployeeComponent } from './components/add-employee/add-employee.com
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StructuralDirectivesComponent } from './components/structural-directives/structural-directives.component';
+import { AttributeDirectiveComponent } from './components/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddEmployeeComponent,EmployeeListComponent, DataBindingComponent, StructuralDirectivesComponent],
+  imports: [
+    RouterOutlet,
+    AddEmployeeComponent,
+    EmployeeListComponent,
+    DataBindingComponent,
+    StructuralDirectivesComponent,
+    AttributeDirectiveComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ learn/src/app/components/attribute-directive/attribute-directive.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-attribute-directive',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './attribute-directive.component.html',
+  styleUrl: './attribute-directive.component.scss'
+})
+export class AttributeDirectiveComponent {
+
+  div1BgColor: string = "";
+  isDiv2Active: boolean = false;
+  num1: string = "";
+  num2: string = "";
+  isActive: boolean = false;
+  ngClassColor: string = "ngClass";
+  backCheck: string = "bc";
+  searchValue: string = '';
+
+  customStyle: any = {
+    'color' : 'green',
+    'background-color' : 'red',
+    'width' : '200px',
+    'height' : '200px',
+    'border-radius':'50%'
+  }
+
+  mySpirit: any[] = [
+    {
+      number:1,totalPower: 7,gender: "male", name:"Pamula",story:"loop",feeling:"creator of everything", alive: true
+    },
+    {
+      number:3,totalPower:3, gender: "male", name:"Prabhu",story:"normal",feeling:"feels unique but nothing", alive: false
+    },
+    {
+      number:7,totalPower: 1,gender: "male", name:"Teja",story:"worst",feeling:"too much depression dead and losers life", alive: true
+    }
+]
+
+  addRedClass(){
+    this.div1BgColor = "bg-danger";
+  }
+
+  addBlueClass(){
+    this.div1BgColor = "bg-primary"
+  }
+
+  toggleDiv2Class(){
+    this.isDiv2Active = !this.isDiv2Active;
+  }
+
+  changeColor(){
+    this.backCheck = this.backCheck === 'bc' ? 'cb' : 'bc';
+      }
+  
+}
